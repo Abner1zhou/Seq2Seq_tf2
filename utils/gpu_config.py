ff1c4d458434c3d49258aca499d55632169113d8
Abner Zhou <abner1zhou@gmail.com>
2020-01-08 20:58:47 +0800

    create gpu_config.py
    Signed-off-by: Abner Zhou <abner1zhou@gmail.com>

---
+++ utils/gpu_config.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 –*-
+"""
+@author: 周世聪
+@contact: abner1zhou@gmail.com 
+@desc: 设置GPU参数
+"""
+import tensorflow as tf
+import os
+
+
+def config_gpu(use_cpu=False):
+    if use_cpu:
+        os.environ['CUDA_VISIBLE_DEVICES'] = '1'  # gpu报错 使用cpu运行
+    else:
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        if gpus:
+            try:
+                tf.config.experimental.set_virtual_device_configuration(
+                    gpus[0],
+                    [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=3000)])
+                logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+                print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+
+                # for gpu in gpus:
+                #     tf.config.experimental.set_memory_growth(gpu, True)
+
+            except RuntimeError as e:
+                print(e)
